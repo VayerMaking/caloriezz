8161f13c0044bf067ed79a660475bdb19d8bf93e
VayerMaking <martin.vayer@gmail.com>
2020-11-13 23:02:34 +0200

    asdf

--- create_tables.sql
+++ create_tables.sql
@@ -1,5 +1,6 @@
 CREATE TABLE Foods(
   id int not null primary key auto_increment,
   food_name varchar(100) not null,
-	quantity int not null
+  quantity_indentifier Enum('kg', 'mg', 'number'),
+  calories int not null
 );
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
+from MySQLdb import escape_string as thwart
+
 import config
 app = Flask(__name__)
 
@@ -14,17 +16,26 @@ mysql = MySQL(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    result = []
     if request.method == "POST":
         details = request.form
         food_name = details['fname']
         quantity = details['quantity']
         quantity_indentifier = details['quantity_indentifier']
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO Foods(food_name, quantity) VALUES (%s, %s)", (food_name, quantity))
+        #cur.execute("INSERT INTO Foods(food_name, quantity, quantity_indentifier) VALUES (%s, %s, %s)", (food_name, quantity, quantity_indentifier))
+        cur.execute('''SELECT food_name FROM Foods''')
+        rv = cur.fetchall()
+        #return str(rv)
+        for row in rv:
+            print(row[0])
+            result.append(row[0])
+            result.append("asdf")
+
         mysql.connection.commit()
         cur.close()
-        return 'success'
-    return render_template('index.html')
+        return result
+    return render_template('index.html', result=result)
 
 
 if __name__ == '__main__':
