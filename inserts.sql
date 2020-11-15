c051ad21cbbd1e0739097ad72d6d0a7174ddf826
VayerMaking <martin.vayer@gmail.com>
2020-11-15 11:28:57 +0200

    smoL changes

--- inserts.sql
+++ inserts.sql
@@ -1,5 +1,5 @@
-INSERT INTO Foods(food_name, quantity_indentifier, calories) VALUES("Steak", "number", 500);
-INSERT INTO Foods(food_name, quantity_indentifier, calories) VALUES("Eggs", "number", 1000);
-INSERT INTO Foods(food_name, quantity_indentifier, calories) VALUES("Apples", "number", 200);
-INSERT INTO Foods(food_name, quantity_indentifier, calories) VALUES("Pears", "number", 255);
-INSERT INTO Foods(food_name, quantity_indentifier, calories) VALUES("Carots", "number", 150);
+INSERT INTO foods(food_name, quantity_indentifier, calories) VALUES("Steak", "number", 500);
+INSERT INTO foods(food_name, quantity_indentifier, calories) VALUES("Eggs", "number", 1000);
+INSERT INTO foods(food_name, quantity_indentifier, calories) VALUES("Apples", "number", 200);
+INSERT INTO foods(food_name, quantity_indentifier, calories) VALUES("Pears", "number", 255);
+INSERT INTO foods(food_name, quantity_indentifier, calories) VALUES("Carots", "number", 150);
--- main.py
+++ main.py
@@ -1,42 +1,68 @@
-from flask import Flask, render_template, request
-from flask_mysqldb import MySQL
-from MySQLdb import escape_string as thwart
+from flask import Flask, render_template, request, flash, redirect, url_for
+#from forms import LoginForm
+from flask_sqlalchemy import SQLAlchemy
 
 import config
 app = Flask(__name__)
 
 
-app.config['MYSQL_HOST'] = config.host
-app.config['MYSQL_USER'] = config.username
-app.config['MYSQL_PASSWORD'] = config.password
-app.config['MYSQL_DB'] = config.db
+app.config['SECRET_KEY'] = config.password
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://vayermaking:vayertues@localhost/caloriezz'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-mysql = MySQL(app)
+db = SQLAlchemy(app)
 
+class foods(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    food_name = db.Column(db.String(100))
+    quantity_indentifier = db.Column(db.Enum('kg', 'mg', 'number'))
+    calories = db.Column(db.Integer())
 
+    def __init__(self, food_name, quantity_indentifier, calories):
+        self.food_name = food_name
+        self.quantity_indentifier = quantity_indentifier
+        self.calories = calories
+
+def check_food_name(food_name, quantity):
+    data = foods.query.all()
+    for i in data:
+        if(food_name == i.food_name):
+            print(i.food_name)
+            print(quantity)
+            print(i.calories)
+            #calculation of calories using quantity and calories for one piece
+            #...
+            #
+
+            result = i.calories * quantity
+
+    return result
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    result = []
+    result = ""
     if request.method == "POST":
         details = request.form
         food_name = details['fname']
         quantity = details['quantity']
         quantity_indentifier = details['quantity_indentifier']
-        cur = mysql.connection.cursor()
-        #cur.execute("INSERT INTO Foods(food_name, quantity, quantity_indentifier) VALUES (%s, %s, %s)", (food_name, quantity, quantity_indentifier))
-        cur.execute('''SELECT food_name FROM Foods''')
-        rv = cur.fetchall()
-        #return str(rv)
-        for row in rv:
-            print(row[0])
-            result.append(row[0])
-            result.append("asdf")
-
-        mysql.connection.commit()
-        cur.close()
-        return result
+        #result  = food_name + quantity_indentifier + str(quantity)
+        #return result
+        #return redirect(url_for('result', result_out=result_out))
+        result = check_food_name(food_name, quantity)
     return render_template('index.html', result=result)
-
+'''
+@app.route('/result/<result_out>')
+def result(result_out):
+    return render_template('result.html', result_out=result_out)
+'''
+@app.route('/asdf', methods=['GET', 'POST'])
+def asdf():
+    data = foods.query.all()
+    for i in data:
+        if(i.food_name == "Eggs"):
+            print(i.food_name)
+            result = i.food_name + i.quantity_indentifier + str(i.calories)
+    return result
 
 if __name__ == '__main__':
     app.run()
