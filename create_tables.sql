9b953933bdbdf3473fa56c54db7f367d1f0742c7
VayerMaking <martin.vayer@gmail.com>
2020-11-16 19:16:25 +0200

    added food categories

--- create_tables.sql
+++ create_tables.sql
@@ -1,5 +1,6 @@
 CREATE TABLE foods(
   id int not null primary key auto_increment,
+  category varchar(100) not null,
   food_name varchar(100) not null,
   quantity_indentifier Enum('kg', 'mg', 'number'),
   calories int not null
